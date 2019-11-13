64285c510b1583eb3acc310b2a3bd3c131f61917
Timbaleek <timbaleek@hotmail.de>
2019-11-13 13:25:19 +0100

    kp

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -43,10 +43,10 @@ public class Player extends CollidingGameEntity{
 	
 	public void update(){
 		if(vel.y <= 0.01f && vel.y >= -0.01f){
-			Main.player.gRect.changeTex(2, 100, true); // change to falling texture
+			//Main.player.gRect.changeTex(2, 100, true); // change to falling texture
 		}
 		if(Main.player.gRect.isDone() == 1){
-			Main.player.gRect.changeTex(1, (long)(jumpForce/(Main.gravity)), false); // change to ascending texture
+			//Main.player.gRect.changeTex(1, (long)(jumpForce/(Main.gravity)), false); // change to ascending texture
 		}
 		super.update();
 	}
